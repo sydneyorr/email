e1cc2f5f6a49f40839d3c28839b40a51e15f7d51
Sydney Orr <sydneywhitters@gmail.com>
2021-12-16 21:03:05 -0700

    emial

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,69 +1,31 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { Image } from "semantic-ui-react"
-
-// Import React FilePond
-import { FilePond, registerPlugin } from "react-filepond";
-
-// Import FilePond styles
-import "filepond/dist/filepond.min.css";
-
-// Import the Image EXIF Orientation and Image Preview plugins
-// Note: These need to be installed separately
-// `npm i filepond-plugin-image-preview filepond-plugin-image-exif-orientation --save`
-import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
-import FilePondPluginImagePreview from "filepond-plugin-image-preview";
-import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
-import JustImageUpload from "./JustImageUpload";
-
-// Register the plugins
-registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
+import emailjs from 'emailjs-com';
 
 export default function Home() {
-  const [url, setUrl] = useState("no pictures")
-  const [caption, setCaption] = useState("");
-  const [files, setFiles] = useState([]);
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log(caption);
-    try {
-      // send image stuff in files state
-      let data = new FormData();
-      data.append("file", files[0].file);
-      data.append("caption", caption);
-      let res = await axios.post("/api/memes", data);
-      console.log(res);
-    } catch (err) {
-      alert("err in post meme occured");
-    }
-  };
 
-  const fileChanged = (fileItems) => {
-    setFiles(fileItems);
-  };
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_8vqwr9n', 'template_sa7vrbe', e.target, 'user_4mGxDrGds7plQG1Re3QGo')
+    .then((result)=> {
+      console.log(result.text);
+    }, (error) => {
+      console.log(error.text);
+    }
+    )
+    e.target.reset();
+  }
+
+
   return (
     <div>
-      <h1>Home</h1>
-      <form onSubmit={handleSubmit}>
-        <p>caption</p>
-        <input
-          type="text"
-          value={caption}
-          onChange={(e) => setCaption(e.target.value)}
-        />
-        <br />
-        <FilePond
-          files={files}
-          allowMultiple={false}
-          // onupdatefiles={setFiles}
-          onupdatefiles={fileChanged}
-          labelIdle='Drag and Drop your files or <span class="filepond--label-action">Browse</span>'
-        />
-        <br />
-        <button type="submit">add</button>
+      <form onSubmit={sendEmail}>
+        <input type="text" placeholder="name" name="name"/>
+        <input type="email" placeholder="email address" name="email"/>
+        <input type="text" placeholder="subject" name="subject"/>
+        <input type="text" placeholder="message" name="your message"/>
+        <button type="submit">submit</button>
       </form>
-      <Image src={url} />
-      <JustImageUpload />
     </div>
   );
 }
